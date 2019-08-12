a881c561d686c39d6885706c9526865935672e91
Colton <cmathews@eidebailly.com>
2019-08-12 17:07:51 -0600

    Refactored Axios Calls; Prepped Endpoint to receive POST from twitch.

--- endpoints.js
+++ endpoints.js
@@ -2,16 +2,23 @@ const express = require('express'),
 app = express();
 
 app.post('/streamstatus', function (req, res) {
-  res.status(200);
-	res.end(JSON.stringify({
-    Message:"Let Do This",
-    Code: 1234
-  }));
+  console.log(">>>>> Request Received at /streamstatus <<<<<");
+  console.log("Full Request: "+req);
+  console.log('Request Challenge: '+req.hub_challenge);
+  if (!req.hub_challenge) {
+    console.log(`New {Published Event} for {StreamID Here}`)
+    //Do something with this information    
+  }
+  else {
+    console.log(`New ${req.hub_mode} Request`)
+    res.status(200);
+    res.end(req.hub_challenge);
+  }
 });
 
-let server = app.listen(8081, function () {
-   let host = server.address().address,
-   port = server.address().port
+let server = app.listen(8881, function () {
+   let host = server.address().address
+   let port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
 })
 // Now try to access defined API using URL: http://127.0.0.1:8081/listUsers and HTTP Method : GET on local machine using any REST client. This should produce following result −
