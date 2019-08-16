51586bb354cb1bb47e9ecf966645fa4cd68112cb
Colton <cmathews@eidebailly.com>
2019-08-16 14:40:58 -0600

    .

--- dispatchers/eventDispatcher.js
+++ dispatchers/eventDispatcher.js
@@ -1,6 +1,6 @@
 //This is an endpoint that handles any published events.
 
-const event = require('../eventhandlers/eventDex')
+const evt = require('../eventhandlers/eventDex')
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
@@ -19,22 +19,19 @@ app.post('/publishevent', function (req, res) {
 
 	if(event.id == '52404043'){
     console.log("SACSIDE Event regestered");
-    event.handleSac(event);
+    evt.handleSac(event);
 	} else if (event.id == "51831700"){
     console.log("MIT Event regestered");
-    event.handleMit(event);
+    evt.handleMit(event);
 	} else if (event.id == "21384358") {
     console.log("PICKLE Event regestered");
-    event.handlePick(event);
+    evt.handlePick(event);
 	} else { //feed these to #technical-testing
     console.log("MISC Event regestered");
-    event.handleMisc(event);
+    evt.handleMisc(event);
 	}
 
-	
-	//window.addEventListener('onError', function(e) {
-	//	channel.send(e.error.message) // or client.sendMessage(channel, e.error.message) in older versions;
-	//});
+
 	
     res.status(200);
     res.write("Hello World");
