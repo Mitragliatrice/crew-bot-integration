7c5e7b4c9c405ab744e208aa0e7727128091de1a
Mit <mathews.colton@gmail.com>
2019-08-16 18:00:33 +0000

    Updated Event Handler with BodyParser

--- eventHandler.js
+++ eventHandler.js
@@ -1,35 +1,38 @@
 //This is an endpoint that handles any published events.
 
-const mit = requires('./eventhandlers/mitevents')
-const sac = requires('./eventhandlers/sacsideevents')
-const pick = requires('./eventhandlers/pickleevents')
-const misc = requires('./eventhandlers/miscevents')
+const mit = require('./eventhandlers/mitevents')
+const sac = require('./eventhandlers/sacsideevents')
+const pick = require('./eventhandlers/pickleevents')
+const misc = require('./eventhandlers/miscevents')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const express = require('express'),
+body = require('body-parser'),
 app = express();
+
+app.use(body.json());
 
 app.post('/publishevent', function (req, res) {
   console.log(">>>>> New Event Via /publishevent <<<<<");
   // console.log(`New {Published Event} for {StreamID Here}`)
-  console.log(req);
+  console.log(req.body);
 
-	let event = req.data;
-	if(data.id == "52404043"){ //Sacside Stream Event
+	let event = req.body.data;
+	if(event.id == "52404043"){ //Sacside Stream Event
     //Do Sacside Things
     console.log("SACSIDE Event regestered");
-    sac.handleEvent(req.data);
-	} else if (data.id == "51831700"){ //Mit Stream Event
+    sac.handleEvent(event);
+	} else if (event.id == "51831700"){ //Mit Stream Event
     // Do Mit Things
     console.log("MIT Event regestered");
-    mit.handleEvent(req.data);
-	} else if (data.id == "21384358") { //Pickle Stream Event
+    mit.handleEvent(event);
+	} else if (event.id == "21384358") { //Pickle Stream Event
 		// Do Pickle Things
     console.log("PICKLE Event regestered");
-    pick.handleEvent(req.data);
+    pick.handleEvent(event);
 	} else { //feed these to #technical-testing
     console.log("MISC Event regestered");
-    misc.handleEvent(req.data);
+    misc.handleEvent(event);
 	}
 
 	
